e326177ee02bbc5b52b21563b1c540e69891d156
anycodef <anycodefast1@gmail.com>
2023-06-23 04:56:08 -0500

    Modify excersice twins primes.

--- primeros_gemelos.cpp
+++ primeros_gemelos.cpp
@@ -5,36 +5,34 @@ using namespace std;
 bool is_prime(int);
 
 int main() {
-  int a, b, c;
+  int a, b;
   int last_number_prime = 0;
   int amount_twis_number = 0;
 
   cout << "\n\tThis program found a twins number between a and b." << endl;
-  cout << " a: ";
-  cin >> a;
-  cout << " b: ";
-  cin >> b; 
+  do {
+    system("cls");
+    cout << "\na: ";
+    cin >> a;
+  } while (a < 0);
+  
+  do {
+    system("cls");
+    cout << "\na: " << a << endl;
+    cout << "b: ";
+    cin >> b; 
+  } while (a > b);
 
-  if ( a != b ) {
-    if ( a > b ) {
-      c = a;
-      a = b;
-      b = c;
-    }
-
-    for ( int current_number = a; current_number <= b; current_number++ ) {
-      if ( is_prime(current_number) ) {
-        if ( last_number_prime ) {
-          if ( current_number - last_number_prime == 2 ) {
-            cout << " twis: " << last_number_prime << " - " << current_number << endl;
-            amount_twis_number ++;
-          }
+ for ( int current_number = a; current_number <= b; current_number++ ) {
+    if ( is_prime(current_number) ) {
+      if ( last_number_prime ) {
+        if ( current_number - last_number_prime == 2 ) {
+          cout << " twis: " << last_number_prime << " - " << current_number << endl;
+          amount_twis_number ++;
         }
-        last_number_prime = current_number;
       }
+      last_number_prime = current_number;
     }
-  } else {
-    cout << "\n\tNot found twins numbers because a is equal to b. ";
   }
 
   cout << "\n\tAmount twis number: " << amount_twis_number << endl;
